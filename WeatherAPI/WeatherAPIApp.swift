90fdffc34b8374c54eac7756d981974e15c5133b
Martyna <mieczkowska.maartyna@gmail.com>
2024-08-29 14:51:40 +0200

    Initial Commit

---
+++ WeatherAPI/WeatherAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPIApp.swift
+//  WeatherAPI
+//
+//  Created by Martyna Mieczkowska on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
